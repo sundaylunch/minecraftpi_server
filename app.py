f3602a8337f45532ff2caa66ce3a9d02f0aa51dc
sundaylunch <sundaylunch@DESKTOP-66V8638>
2025-12-25 21:46:03 +0000

    update

--- app.py
+++ app.py
@@ -12,33 +12,61 @@ mc = None
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/chat")
-def chat():
+def try_to_connect():
     global mc
+
     tries=0
     while mc == None:
         try:
             tries+=1
             mc = minecraft.Minecraft.create()
-            print("Connected tp "+mc)
+            print("Connected to "+mc)
+            return True
         except:
-            print("Failed to connect")
+            print("Failed to connect retry..")
             if tries<3:
-                pass
-            message={"msg":"Failed to connect to minecraft at localhost"}
-            return jsonify(message)
+                continue
+            else:
+                print("Failed to connect")
+                return False
+  
+
+@app.route("/chat")
+def chat():
+    global mc
+    if mc==None:
+           if try_to_connect()==False:
+               message={"msg":"Failed to connect to minecraft at localhost"}
+               return jsonify(message)
+           
     msg = request.args.get("msg", "Hello from scratcher!")
-    mc.chat(msg)
+    mc.postToChat(msg)
     message={"success": True, "msg":"chat posted to minecraft at localhost"}
     return jsonify(message)
 
 @app.route("/getBlock")
 def getBlock():
+    global mc
+    if mc==None:
+           if try_to_connect()==False:
+               message={"msg":"Failed to connect to minecraft at localhost"}
+               return jsonify(message)
+    
+    
     x=request.args.get("x")
     y=request.args.get("y")
     z=request.args.get("z")
-    print(x,y,z)
-    message={"id":1}
+    
+    try:
+         x=float(x)
+         y=float(y)
+         z=float(z)
+
+         id=mc.getBlock(x,y,z)
+         print(f"Got block at {x}, {y}, {z} as {id}")
+         message={"id":id}
+    except:
+         message={"msg":"invalid co-ordinates"}
     return jsonify(message)
 
 
